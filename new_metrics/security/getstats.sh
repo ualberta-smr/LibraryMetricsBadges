1de6c78459dca1caf8fbf151e8867be70a5ebf53
bui1@ualberta.ca <bui1@ualberta.ca>
2018-10-16 18:59:28 -0600

    Minor change to fix find file command. Exclude maven and gradle files also

--- new_metrics/security/getstats.sh
+++ new_metrics/security/getstats.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 # #referencing: https://github.com/bui1/librarycomparisonswebsite/blob/master/scripts/breakingchanges/cloneRepo.sh
-rm -rf "repos"
-mkdir "repos"
-cd "repos"
-while read line 
-do  
-	git clone "$line" 
-done < $1
+# rm -rf "repos"
+# mkdir "repos"
+# cd "repos"
+# while read line 
+# do  
+# 	git clone "$line" 
+# done < $1
 
 # after clone,
 # need to go to every directory and detect if pom.xml or grade.build file
@@ -16,10 +16,11 @@ done < $1
 # else
     # gradle build -x test
 
+cd "repos"
 for library in ls -d */; do
-    check = "$(find "$library" -maxDepth 1 -type f -name 'pom.xml' -printf .)"
-    cd library
-    if [$check -eq 1]
+    check = "$(find "$library" -maxdepth 1 -type f -name 'pom.xml')"
+    cd $library
+    if [$check]
     then    
         mvn clean install -DskipTests=true
     else
