9d2f0d5b1300acf74b09e39f21a554aee0a8c857
bui1@ualberta.ca <bui1@ualberta.ca>
2018-10-22 11:59:51 -0600

    Add new sqlite code to updatestats script. Still need to fix some small issues

--- new_metrics/security/securitybadge.js
+++ new_metrics/security/securitybadge.js
@@ -11,42 +11,29 @@ module.exports = function(req,res){
     return new Promise((resolve, reject) => {
         // due to Github request cache limits, badge would only be updated once per day
         // running the shell script to update is slow 
-        // exec("bash updatestats.sh",  {cwd: './new_metrics/security'}, function(err,stdout,stderr) {
-        //     if (err){
-        //         console.log(err);
-        //         return reject(err.code);
-        //     }
+        exec("bash updatestats.sh",  {cwd: './new_metrics/security'}, function(err,stdout,stderr) {
+            if (err){
+                console.log(err);
+                return reject(err.code);
+            }
 
-        //     const numberofbugs = fs.readFileSync(filePathCurrentBugs, 'utf8');
-        //     const previousbugs = fs.readFileSync(filePathPrevBugs,'utf8');
+            const numberofbugs = fs.readFileSync(filePathCurrentBugs, 'utf8');
+            const previousbugs = fs.readFileSync(filePathPrevBugs,'utf8');
 
-        //     let bugString = "";
-        //     if (previousbugs.length == 0 || previousbugs == numberofbugs){
-        //         bugString = numberofbugs.concat(" ", "nochange");
-        //     }
-        //     else if(previousbugs < numberofbugs){
-        //         bugString = numberofbugs.concat(" ", "↑");
-        //     }
-        //     else{
-        //         bugString = numberofbugs.concat(" ", "↓");
-        //     }
+            let bugString = "";
+            if (previousbugs.length == 0 || previousbugs == numberofbugs){
+                bugString = numberofbugs.concat(" ", "--");
+            }
+            else if(previousbugs < numberofbugs){
+                bugString = numberofbugs.concat(" ", "↑");
+            }
+            else{
+                bugString = numberofbugs.concat(" ", "↓");
+            }
 
-        //     return resolve(bugString);
-        // });
-        const numberofbugs = fs.readFileSync(filePathCurrentBugs, 'utf8');
-        const previousbugs = fs.readFileSync(filePathPrevBugs,'utf8');
-
-        let bugString = "";
-        if (previousbugs.length == 0 || previousbugs == numberofbugs){
-            bugString = numberofbugs.concat(" ", "--");
-        }
-        else if(previousbugs < numberofbugs){
-            bugString = numberofbugs.concat(" ", "↑");
-        }
-        else{
-            bugString = numberofbugs.concat(" ", "↓");
-        }
-
-        return resolve(bugString);
+            return resolve(bugString);
+        });
     });
+
+    // run sqlite3 to grab latest data for badge 
 };
--- new_metrics/security/updatestats.sh
+++ new_metrics/security/updatestats.sh
@@ -1,21 +1,46 @@
 # Assuming java classes don't have to be compiled, run this script to get spotbugs data
 # bash updatestats.sh
 
+# cd "repos" 
+# for library in $( ls -d */ | sed 's#/##' )
+# do
+#     cd $library
+#     if cat "../../numberofbugs.txt";
+#     then
+#         cat "../../numberofbugs.txt" > "../../previous_stat.txt"
+#     fi
+#     # run spotbugs with findsecbugs plugin to find all security vulnerabilities with highest confidence level
+#     java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > ../../bugs.txt
+#     cd ..
+#     wc -l ../bugs.txt | grep -o "[0-9]\+" > ../numberofbugs.txt
+# done
+
+
 cd "repos" 
 for library in $( ls -d */ | sed 's#/##' )
 do
     cd $library
-    if cat "../../numberofbugs.txt";
-    then
-        cat "../../numberofbugs.txt" > "../../previous_stat.txt"
+    if sqlite3 ../../bugs.db "select * from bugs where libname = \"${library}\";";
+    then   
+        prevnum=$(if sqlite3 ../../bugs.db "select * from bugs where libname = \"${library}\";")
+        java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > "../../bugs.txt"
+        updatednum=$(wc -l ../../bugs.txt | grep -o "[0-9]\+")
+
+        if $prevnum < $updatednum;
+        then  
+            sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '↑' WHERE libname = \"${library}\";"
+        elif $prevnum > $updatednum
+        then
+            sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '↓' WHERE libname = \"${library}\";"
+        else    
+            sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '--' WHERE libname = \"${library}\";"
+        fi  
+    else
+        java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > "../../bugs.txt"
+        newnum=$(wc -l ../../bugs.txt | grep -o "[0-9]\+")
+        sqlite3 ../../bugs.db "INSERT INTO bugs VALUES (NULL, libname = \"${library}\", numberofbugs = \"${newnum}\", status = '--');"
     fi
-    # run spotbugs with findsecbugs plugin to find all security vulnerabilities with highest confidence level
-    java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > ../../bugs.txt
-    cd ..
-    wc -l ../bugs.txt | grep -o "[0-9]\+" > ../numberofbugs.txt
 done
-
-
 
 # select * from bugs where libname=$library
 # grab the result from ^^ and save it to a variable
