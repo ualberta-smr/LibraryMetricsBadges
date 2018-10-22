22c1596de22f01c8a165402634fb7975a09c7d99
bui1@ualberta.ca <bui1@ualberta.ca>
2018-10-22 12:43:10 -0600

    Fix integer type issue for conditional checking any stat changes
    Issue is due to wrong query to grab the last saved number of bugs

--- new_metrics/security/updatestats.sh
+++ new_metrics/security/updatestats.sh
@@ -22,20 +22,24 @@ do
     cd $library
     if [[ $(sqlite3 ../../bugs.db "select * from bugs where libname = \"${library}\";") ]];
     then   
-        prevnum=$(if sqlite3 ../../bugs.db "select * from bugs where libname = \"${library}\";")
+        echo "stat being updated for \"${library}\""
+
+        prevnum=$(sqlite3 ../../bugs.db "select numberofbugs from bugs where libname = \"${library}\";")
         java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > "../../bugs.txt"
         updatednum=$(wc -l ../../bugs.txt | grep -o "[0-9]\+")
 
-        if [ "$prevnum" < "$updatednum" ];
+        if [ $prevnum -lt $updatednum ];
         then  
             sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '↑' WHERE libname = \"${library}\";"
-        elif [ "$prevnum" > "$updatednum" ]
+        elif [ $prevnum -gt $updatednum ]
         then
             sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '↓' WHERE libname = \"${library}\";"
         else    
             sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '--' WHERE libname = \"${library}\";"
         fi  
     else
+        echo "stat being newly inserted for \"${library}\""
+        
         java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > "../../bugs.txt"
         newnum=$(wc -l ../../bugs.txt | grep -o "[0-9]\+")
         sqlite3 ../../bugs.db "INSERT INTO bugs (id, libname, numberofbugs, status) VALUES (NULL, \"${library}\", \"${newnum}\", '--');"
