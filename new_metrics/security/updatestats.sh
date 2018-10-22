4923291841c299dfabfa2eb080443d7fe66c6c7b
bui1@ualberta.ca <bui1@ualberta.ca>
2018-10-22 12:27:05 -0600

    Fix insert table problem

--- new_metrics/security/updatestats.sh
+++ new_metrics/security/updatestats.sh
@@ -20,16 +20,16 @@ cd "repos"
 for library in $( ls -d */ | sed 's#/##' )
 do
     cd $library
-    if sqlite3 ../../bugs.db "select * from bugs where libname = \"${library}\";";
+    if [[ $(sqlite3 ../../bugs.db "select * from bugs where libname = \"${library}\";") ]];
     then   
         prevnum=$(if sqlite3 ../../bugs.db "select * from bugs where libname = \"${library}\";")
         java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > "../../bugs.txt"
         updatednum=$(wc -l ../../bugs.txt | grep -o "[0-9]\+")
 
-        if $prevnum < $updatednum;
+        if [ "$prevnum" < "$updatednum" ];
         then  
             sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '↑' WHERE libname = \"${library}\";"
-        elif $prevnum > $updatednum
+        elif [ "$prevnum" > "$updatednum" ]
         then
             sqlite3 ../../bugs.db "UPDATE bugs SET numberofbugs = \"${updatednum}\", status = '↓' WHERE libname = \"${library}\";"
         else    
@@ -38,20 +38,7 @@ do
     else
         java -jar ../../spotbugs-3.1.3/lib/spotbugs.jar -textui -include ../../includefilterfile.xml -effort:max -high -pluginList ../../findsecbugs-plugin-1.8.0.jar $library > "../../bugs.txt"
         newnum=$(wc -l ../../bugs.txt | grep -o "[0-9]\+")
-        sqlite3 ../../bugs.db "INSERT INTO bugs VALUES (NULL, libname = \"${library}\", numberofbugs = \"${newnum}\", status = '--');"
+        sqlite3 ../../bugs.db "INSERT INTO bugs (id, libname, numberofbugs, status) VALUES (NULL, \"${library}\", \"${newnum}\", '--');"
     fi
 done
 
-# select * from bugs where libname=$library
-# grab the result from ^^ and save it to a variable
-# now run spotbugs and getbugs result to save to variable
-# save variable into table by update
-# sqlite3 bugs.db "SOME COMMAND;" ".exit" > test.txt
-
-# if sqlite3 ../../bugs.db 'select * from bugs where libname = \"${SOMEVAR}\"'; # that means the libary exists in the table
-    # previousnumber = savedValue ^^
-    # run spotbugs and save to variable
-    # compare previousnumber with newnumber and save to variable
-    # update SQL table for new variable so libname=$library, numberofbugs = newnumber , update = "--, down, or up arrow"
-# else
-    # lib doesnt exists so we run spotbugs and insert new value into t
