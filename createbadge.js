6069da68077c1931ee7e9a0130727710f701b666
bui1@ualberta.ca <bui1@ualberta.ca>
2018-11-05 16:33:34 -0700

    JsDoc addition to express middleware function for our server

--- createbadge.js
+++ createbadge.js
@@ -11,11 +11,34 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+/**
+ * Reference for how to document Express middleware functions from Stack Overflow
+ * https://stackoverflow.com/a/36409525
+ * jeff_mcmahan
+ * https://stackoverflow.com/users/2757539/jeff-mcmahan
+ * user contributions licensed under cc by-sa 3.0 with attribution required. rev 2018.11.5.32076
+ */
+
+/**
+ * Home Page of Server
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ *  
+ * @returns {undefined}
+ */
 app.get('/', (req, res) => {
     res.send("Welcome to Software Metrics Research Page");
 });
 
-// Update for security badge will be done seperately with updatestats.sh due to heavy shell processing with Spotbugs run
+/**
+ * Get Security endpoint for results of Spotbugs run
+ * Disclaimer: Update for security badge will be done seperately with updatestats.sh due to heavy shell processing with Spotbugs run
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ *  
+ * @returns {undefined}
+ */
 app.get('/security', (req, res) => {
     getSecurity(req,res)
         .then(result => {
@@ -29,6 +52,14 @@ app.get('/security', (req, res) => {
         }); 
 });
 
+/**
+ * Get Release Frequency endpoint for average number of days between releases
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ *  
+ * @returns {undefined}
+ */
 app.get('/releasefreq', (req, res) => {
     getRelease(req,res)
         .then(result => {
@@ -47,6 +78,14 @@ app.get('/releasefreq', (req, res) => {
         });
 });
 
+/**
+ * Get Last Discussed on Stack Overflow endpoint which outputs last date discussed
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ *  
+ * @returns {undefined}
+ */
 app.get('/lastdiscussed', async (req, res) => {
     await getLastDiscussed(req,res)
         .then(date => {
@@ -60,6 +99,14 @@ app.get('/lastdiscussed', async (req, res) => {
         }); 
 });
 
+/**
+ * Get Pull Requests endpoint that calculates percentage of merged contributor PRs 
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ *  
+ * @returns {undefined}
+ */
 app.get('/pullrequests', async(req, res) => {
     await getPRs(req,res)
         .then(percentage => {
@@ -73,6 +120,14 @@ app.get('/pullrequests', async(req, res) => {
         });
 });
 
+/**
+ * Get Issue Response endpoint that shows average number of days for first response to an issue
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ *  
+ * @returns {undefined}
+ */
 app.get('/issueresponse', async (req, res) => {
     await getIssueResponseTime(req,res)
         .then(responseTime => {
