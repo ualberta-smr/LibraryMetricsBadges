a4f991798a56bff8cc307e38049be471b1581f3c
bui1@ualberta.ca <bui1@ualberta.ca>
2018-11-19 17:17:26 -0700

    get started with using cache results and search api for PR

--- existing_metrics/issue_response_time/issueresponsetime.js
+++ existing_metrics/issue_response_time/issueresponsetime.js
@@ -8,6 +8,15 @@ const dbpath = path.resolve(__dirname, "../../badges.db");
 const db = new sqlite3.Database(dbpath);
 const graphQLLink = "https://api.github.com/graphql";
 
+/**
+ * Calculates the issue response time metric by using a GraphQL call to get issues with comments
+ * at the same time instead of 2 separate calls with REST.
+ * Consider only issues whose first comment author != author of issue
+ * 
+ * @param {Object} req - Express request object
+ * 
+ * @returns {Array} 3 items, number of all issues, number of issues that have valid comments, and calculated metric
+ */
 let calculateMetric = async(owner,libName) => {
     // How to Call a GraphQL endpoint using Axios (HTTP client) -- Medium
     // https://medium.com/@stubailo/how-to-call-a-graphql-server-with-axios-337a94ad6cf9
@@ -15,9 +24,9 @@ let calculateMetric = async(owner,libName) => {
 
     let cursor = null
     let next = true;
-    let totalMS = 0;
-    let totalIssues = 0;
-    let validIssues = 0;
+    let totalMS = 0;        // total milliseconds of all calculated differences between issue creation date and issue comment date
+    let totalIssues = 0;    // all issues
+    let validIssues = 0;    // issues that match our criteria
     let state = "OPEN";
     while (next){
         try{
@@ -70,6 +79,8 @@ let calculateMetric = async(owner,libName) => {
 
                 // to make sure we dont account for deleted accounts
                 if(element.node.author !== null && element.node.comments.edges.length > 0){
+
+                    // check if a comment exists and if the authors are not equal
                     if (typeof element.node.comments.edges[0] !== "undefined"
                         && (element.node.author.login !== element.node.comments.edges[0].node.author.login)){
                         const issueCreation = moment(new Date(element.node.createdAt)).valueOf();
@@ -101,9 +112,19 @@ let calculateMetric = async(owner,libName) => {
     } 
     console.log(totalIssues, validIssues);
 
-    return Math.floor((totalMS / validIssues / 86400000));
+    return [validIssues, totalIssues, Math.floor((totalMS / validIssues / 86400000))];
 };
 
+/**
+ * GET endpoint to get average number of days for first issue comment that 
+ * is authored by someone not the issue creator
+ * 
+ * @param {Object} req - Express request object
+ *  
+ * @returns {number} average number of days for issue response time
+ * 
+ * @example localhost:3000/issueresponse?owner=axios&libname=axios
+ */
 module.exports = async (req) => {
     let owner = req.query.owner;
     let libName = req.query.libname;
@@ -117,7 +138,33 @@ module.exports = async (req) => {
         try{
             let response = await calculateMetric(owner,libName);
             console.log(response);
-            resolve(response);
+
+            await db.get(`SELECT * from issueresponse where libname="${libName}";`, async (err, row) => {
+                if (err){
+                    reject(err);
+                }
+                let status = "--";
+                if (typeof row !== "undefined"){
+                    if (row.averagedays < response[2]){
+                        status = "↑";
+                    }
+                    else if (row.averagedays > response[2]){
+                        status = "↓";
+                    }
+                    else{
+                        status = "--";
+                    }
+                }
+                let query = `INSERT OR REPLACE INTO issueresponse(libname, averagedays, totalWithComments, totalIssues, status) VALUES (?,?,?,?,?);`;
+                try{
+                    await db.run(query, [libName, ...response, status]);
+                }
+                catch(err){
+                    return reject(err);
+                }
+
+            });
+            resolve(response[2]);
         }
         catch(err){
             reject(err);
